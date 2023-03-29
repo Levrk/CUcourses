fe9b5ee86e93d55c724b996edc100a2666854981
Levrk <levrolandkalb@gmail.com>
2023-03-29 17:11:16 -0400

    Updated create review form/view/template

--- reviews/forms.py
+++ reviews/forms.py
@@ -7,11 +7,17 @@ class ReviewForm(forms.ModelForm):
     instructor = forms.CharField(label="Instructor Name")
     reviewText = forms.CharField(label="Review Text")
     anon = forms.BooleanField(label="Anonymous", required=False)
+    textbook = forms.BooleanField(label="Textbook Required", required=False)
+    OPTIONS = [(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')]
+    fun = forms.ChoiceField(choices=OPTIONS, widget=forms.CheckboxInput)
+    interesting = forms.ChoiceField(choices=OPTIONS, widget=forms.CheckboxInput)
+    difficult = forms.ChoiceField(choices=OPTIONS, widget=forms.CheckboxInput)
     
     class Meta:
         model = review
-        fields = [ 'course', 'instructor', 'reviewText', 'anon']
+        fields = [ 'course', 'instructor', 'reviewText', 'anon','textbook','fun','interesting','difficult']
         widgets = {
-            'anon': forms.TextInput(attrs={'class': 'left-label'})
+            'anon': forms.TextInput(attrs={'class': 'left-label'}),
+            'textbook': forms.TextInput(attrs={'class': 'left-label'})
         }
         #labels = {'course':'CourseCode', 'instructor': 'Professor','reviewText': 'Review', 'anon':'Post Anonymously'}
